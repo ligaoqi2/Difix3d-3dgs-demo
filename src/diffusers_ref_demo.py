beabc4a138f0210a82251f7e61fc5ce107262d3f
ligaoqi <ligaoqi02@gmail.com>
2025-06-13 12:49:10 +0800

    init:lgq

---
+++ src/diffusers_ref_demo.py
@@ -0,0 +1,12 @@
+from pipeline_difix import DifixPipeline
+from diffusers.utils import load_image
+
+pipe = DifixPipeline.from_pretrained("nvidia/difix_ref", trust_remote_code=True)
+pipe.to("cuda")
+
+input_image = load_image("assets/example_input.png")
+ref_image = load_image("assets/example_ref.png")
+prompt = "remove degradation"
+
+output_image = pipe(prompt, image=input_image, ref_image=ref_image, num_inference_steps=1, timesteps=[199], guidance_scale=0.0).images[0]
+output_image.save("example_output.png")
